feefb29d276ead49d25f6e9a7c8d28612802140e
Vivek Kalyan <hello@vivekkalyan.com>
2025-08-04 16:06:14 -0700

    feat: Create a generic task class

---
+++ dev/multi-task/task.py
@@ -0,0 +1,32 @@
+from typing import List, Generator, TypeVar, Generic
+import art
+
+TScenario = TypeVar('TScenario')
+
+class Task(Generic[TScenario]):
+    def __init__(self, name: str):
+        self.name = name
+
+    def get_dataset(self, split: str) -> Generator[TScenario, None, None]:
+        """
+        Returns a generator of scenarios for the given split (e.g., 'train', 'val', 'test').
+        Each scenario contains all data needed to run a rollout.
+        This method must be implemented by subclasses.
+        """
+        raise NotImplementedError
+
+    async def run(
+        self, 
+        model: art.Model, 
+        scenario: TScenario,
+        num_samples: int = 1
+    ) -> List[art.Trajectory]:
+        """
+        Run model on scenarios and return trajectories with rewards.
+        
+        - Can be single scenario -> single trajectory (just loop internally)
+        - Can be batch of scenarios -> batch of trajectories
+        - Can evaluate rewards immediately or use LLM judge on the batch
+        - Can be single-turn or multi-turn
+        """
+        raise NotImplementedError
